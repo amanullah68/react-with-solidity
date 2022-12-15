501cfa85f4eb424cdd49ac7f84505d2411444804
Aman <aman.zahoor68@gmail.com>
2022-12-15 16:37:29 +0400

    Initialize

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="App">
+      <h1 className="text-10xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
